70b7e5027bebf262ab81c4f1ba325731f6e33844
Thomas <tzhang12062004@gmail.com>
2025-04-22 18:27:38 -0400

    imported libraries and seeded code

--- code.py
+++ code.py
@@ -1,6 +1,10 @@
-#N number : 10992530
-
 import numpy as np
 import pandas as pd 
 from scipy import stats
-import matplotlib.pyplot
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import random
+
+random.seed(10992530)
+
